e380104d486b6a9cceabd7bfad239afc3c3d29c8
lively-bundle-0725 <tbandachit@gmail.com>
2020-02-10 03:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal(){
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   if (animal == "dog")
-    animal == "cat"
+    animal = "cat"
   return animal
 }
 
